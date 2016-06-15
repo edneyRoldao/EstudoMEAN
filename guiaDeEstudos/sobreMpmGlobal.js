053684018e08d6e6befc8ac7d1ee947aa877f16d
edneyRoldao <edneyroldao@gmail.com>
2016-06-15 16:51:40 -0300

    adicionada lista com os dependências do projeto que devem ser instaladas globalmente

--- guiaDeEstudos/sobreMpmGlobal.js
+++ guiaDeEstudos/sobreMpmGlobal.js
@@ -1,10 +1,20 @@
-npm install nodemon -g
-npm install bower -g
-npm install grunt-cli -g
-npm install karma-cli -g
-npm install protractor -g
-npm install
-npm install
-npm install
-npm install
+/*
+	Edney Roldão - 14/06/2016
+	- Lista de dependencias que devem ser instaladas globalmente.
 
+		npm install nodemon -g
+		npm install bower -g
+		npm install grunt-cli -g
+		npm install karma-cli -g
+		npm install protractor -g
+		npm install jasmine-core -g
+
+
+	Well, we must to have administrator permission when we go to install dependencies through npm and each
+	OS, this process is different, so, follow the steps below for each one:
+
+	Windows: This is the most simple: Run cmd as administrator and have fun.
+
+	MAC: There are two ways, click on link bellow and watch the video.
+
+*/
